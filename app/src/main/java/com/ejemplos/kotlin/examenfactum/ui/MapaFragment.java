bc2f7a64f3d8d5d731eee71ceaaffd4fec7a75d3
jonathan <jonathanaparicio182@gmail.com>
2021-10-21 22:58:41 -0500

    Creación del Fragmento con el mapa e implementación de un marcador sencillo

--- app/src/main/java/com/ejemplos/kotlin/examenfactum/ui/MapaFragment.java
+++ app/src/main/java/com/ejemplos/kotlin/examenfactum/ui/MapaFragment.java
@@ -1,27 +1,54 @@
 package com.ejemplos.kotlin.examenfactum.ui;
 
-import android.os.Bundle;
-
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.ejemplos.kotlin.examenfactum.R;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapaFragment extends Fragment {
+    GoogleMap mapa;
+    //callback para controlar los eventos y las interacciones en el mapa
+    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+        @Override
+        public void onMapReady(GoogleMap googleMap) {
+            mapa = googleMap;
+            LatLng sydney = new LatLng( -34, 151 );
+            mapa.addMarker( new MarkerOptions()
+                    .position( sydney )
+                    .title( "Marker in Sydney" )
+                    //cambia el color del ícono a mostrar
+                    .icon( BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE) ) );
+            mapa.moveCamera( CameraUpdateFactory.newLatLng( sydney ) );
+        }
+    };
 
-    public MapaFragment() {
-        // Required empty public constructor
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        return inflater.inflate( R.layout.fragment_mapa, container, false );
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        final View root =  inflater.inflate( R.layout.fragment_mapa, container, false );
-
-        return root;
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated( view, savedInstanceState );
+        //permite que el fragmento administre el elemento de GoogleMap
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById( R.id.map );
+        if (mapFragment != null) {
+            mapFragment.getMapAsync( callback );        //habilita el callback para manipular el mapa
+        }
     }
 }
